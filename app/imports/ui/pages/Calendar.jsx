2c6930716b26f81cc89ab08e9d0815a830099d17
audreysoares <amsoares@hawaii.edu>
2023-04-08 13:28:10 -1000

    Update Calendar.jsx

--- app/imports/ui/pages/Calendar.jsx
+++ app/imports/ui/pages/Calendar.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import FullCalendar from '@fullcalendar/react'; // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid';
+import { Button, Container } from 'react-bootstrap';
 
 const Calendar = () => (
-  <FullCalendar
-    plugins={[dayGridPlugin]}
-    initialView="dayGridMonth"
-  />
+  <Container>
+    <FullCalendar
+      plugins={[dayGridPlugin]}
+      initialView="dayGridMonth"
+      // placeholder
+      events={[
+        { title: 'event 1', date: '2023-04-01' },
+        { title: 'event 2', date: '2023-04-02' },
+      ]}
+    />
+    <Button>Add Session</Button>
+  </Container>
 );
 
 export default Calendar;
