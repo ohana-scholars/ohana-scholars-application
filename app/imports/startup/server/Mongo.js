81fdfad2cc4268eb88aaba51ec26b4c10c3341ab
Christopher Pascual <caspascual1293@gmail.com>
2023-04-08 09:58:05 -1000

    Attempting to create student collection

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -2,7 +2,19 @@ import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
 import { Student } from '../../api/student/Student';
 
-/* eslint-disable no-console */
+// Initialize the database with a default data document.
+const addStudent = (student) => {
+  console.log(`  Adding student: ${student.name} `);
+  Student.collection.insert(student);
+};
+
+// Initialize the StudentCollection if empty.
+if (Student.collection.find().count() === 0) {
+  if (Meteor.settings.defaultStudent) {
+    console.log('Creating student data.');
+    Meteor.settings.defaultStudent.forEach(student => addStudent(student));
+  }
+}
 
 // Initialize the database with a default data document.
 const addData = (data) => {
@@ -15,19 +27,5 @@ if (Stuffs.collection.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.forEach(data => addData(data));
-  }
-}
-
-// Initialize the database with a default data document.
-const addStudent = (data) => {
-  console.log(`  Adding: ${data.name} (${data.owner})`);
-  Student.collection.insert(data);
-};
-
-// Initialize the StudentCollection if empty.
-if (Student.collection.find().count() === 0) {
-  if (Meteor.settings.defaultStudent) {
-    console.log('Creating student data.');
-    Meteor.settings.defaultStudent.forEach(data => addStudent(data));
   }
 }
