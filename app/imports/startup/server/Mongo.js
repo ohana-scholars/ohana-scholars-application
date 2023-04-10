6c6a46c0a379ba3449ea9d7e8c2f6b023c588f1d
tungxn <tungxn@hawaii.edu>
2023-04-10 03:59:26 -1000

    Created email collection

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
+import { Emails } from '../../api/email/Email';
 
 /* eslint-disable no-console */
 
@@ -14,5 +15,18 @@ if (Stuffs.collection.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.forEach(data => addData(data));
+  }
+}
+
+const addEmail = (email) => {
+  console.log(`  Adding: ${email.title} (${email.sender})`);
+  Emails.collection.insert(email);
+};
+
+// Initialize the CoursesCollection if empty
+if (Emails.collection.find().count() === 0) {
+  if (Meteor.settings.emails) {
+    console.log('Adding Emails.');
+    Meteor.settings.emails.forEach(email => addEmail(email));
   }
 }
---
+++ app/imports/ui/components/EmailItem.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+const EmailItem = ({ email }) => (
+  <tr>
+    <td>{email.title}</td>
+    <td>{email.sender}</td>
+    <td>
+      <Link to={`/edit/${email._id}`}>Edit</Link>
+    </td>
+  </tr>
+);
+
+// Require a document to be passed to this component.
+EmailItem.propTypes = {
+  email: PropTypes.shape({
+    sender: PropTypes.string,
+    receiver: PropTypes.string,
+    date: PropTypes.string,
+    title: PropTypes.string,
+    body: PropTypes.string,
+    timePlace: PropTypes.string,
+    _id: PropTypes.string,
+  }).isRequired,
+};
+
+export default EmailItem;
