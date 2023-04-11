5b997c1ae6534fc4f7808c65cd051929d61b133a
Christopher Pascual <caspascual1293@gmail.com>
2023-04-10 15:34:46 -1000

    Added publication

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
 import { Stuffs } from '../../api/stuff/Stuff';
+import { Sessions } from '../../api/sessions/Sessions';
 
 // User-level publication.
 // If logged in, then publish documents owned by this user. Otherwise publish nothing.
@@ -12,11 +13,25 @@ Meteor.publish(Stuffs.userPublicationName, function () {
   return this.ready();
 });
 
+Meteor.publish(Sessions.userPublicationName, function () {
+  if (this.userId) {
+    return Sessions.collection.find();
+  }
+  return this.ready();
+});
+
 // Admin-level publication.
 // If logged in and with admin role, then publish all documents from all users. Otherwise publish nothing.
 Meteor.publish(Stuffs.adminPublicationName, function () {
   if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
     return Stuffs.collection.find();
+  }
+  return this.ready();
+});
+
+Meteor.publish(Sessions.adminPublicationName, function () {
+  if (this.userId) {
+    return Sessions.collection.find();
   }
   return this.ready();
 });
