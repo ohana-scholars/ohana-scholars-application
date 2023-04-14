56602c5a04abd057387fa88f63994b66b8abc3d5
Christopher Pascual <caspascual1293@gmail.com>
2023-04-13 17:02:46 -1000

    Attempting to add filter function

--- app/imports/ui/pages/ListCourses.jsx
+++ app/imports/ui/pages/ListCourses.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Col, Container, Row, Table } from 'react-bootstrap';
+import { Col, Container, Row, Table, Form } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
+import _ from 'underscore';
 import LoadingSpinner from '../components/LoadingSpinner';
 import { Courses } from '../../api/courses/Courses';
 import Course from '../components/Course';
 
+// function setToggle(toggle) {
+//   return !toggle;
+// }
+
 /* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 const ListCourses = () => {
+  const [toggle, setToggle] = useState(false);
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
-  const { ready, courses } = useTracker(() => {
+  const { ready, courses } = useTracker(() => { // add back courses
     // Note that this subscription will get cleaned up
     // when your component is unmounted or deps change.
     // Get access to Stuff documents.
@@ -18,11 +24,46 @@ const ListCourses = () => {
     const rdy = subscription.ready();
     // Get the Stuff documents
     const courseItems = Courses.collection.find({}).fetch();
+    // const filterdItems = _.filter(courseItems, function (item) { return item[type].includes(searchInput); });
     return {
       courses: courseItems,
       ready: rdy,
     };
   }, []);
+
+  let type = '';
+  let searchInput = '';
+  const filter = (collection) => {
+    if (type === '' || searchInput === '') {
+      return collection;
+    }
+    return _.filter(collection, function (item) { return item.subject.includes('Algo'); });
+  };
+
+  let filteredItems = filter(courses);
+
+  if (toggle === false) {
+    type = '';
+    searchInput = '';
+  }
+
+  const setSearch = (string) => {
+    searchInput = string;
+  };
+
+  const handleChange = (event) => {
+    filteredItems = filter(courses);
+    setSearch(event.target.value);
+    console.log(`Type:${type}`);
+    console.log(`Search:${searchInput}`);
+    console.log(filteredItems);
+  };
+
+  function setType(event) {
+    const { value } = event.target;
+    type = value;
+  }
+
   return (ready ? (
     <Container className="py-3">
       <Row className="justify-content-center">
@@ -30,6 +71,64 @@ const ListCourses = () => {
           <Col className="text-center">
             <h2>List Courses</h2>
           </Col>
+          <Row className="justify-content-center">
+            <Col>
+              <Form>
+                <Form.Check
+                  type="switch"
+                  id="filter-switch"
+                  label="Filter"
+                  onClick={() => setToggle(!toggle)}
+                />
+              </Form>
+            </Col>
+            <Col>
+              {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+              <label>
+                <input
+                  type="radio"
+                  name="type"
+                  value="subject"
+                  onChange={setType}
+                  disabled={!toggle}
+                />{' '}
+                Subject
+              </label>
+            </Col>
+            <Col>
+              {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+              <label>
+                <input
+                  type="radio"
+                  name="type"
+                  value="title"
+                  onChange={setType}
+                  disabled={!toggle}
+                />{' '}
+                Title
+              </label>
+            </Col>
+            <Col>
+              {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+              <label>
+                <input
+                  type="radio"
+                  name="type"
+                  value="name"
+                  onChange={setType}
+                  disabled={!toggle}
+                />{' '}
+                Name
+              </label>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form>
+                <Form.Control type="text" placeholder="Search" disabled={!toggle} onChange={handleChange} />
+              </Form>
+            </Col>
+          </Row>
           <Table striped bordered hover>
             <thead>
               <tr>
@@ -38,7 +137,7 @@ const ListCourses = () => {
               </tr>
             </thead>
             <tbody>
-              {courses.map((course) => <Course key={course._id} course={course} />)}
+              {filteredItems.map((course) => <Course key={course._id} course={course} />)}
             </tbody>
           </Table>
         </Col>
