e1d0697ca31cdbd74aa4dc82fb558ee0898b7f69
aldenparoni <aparoni@hawaii.edu>
2023-04-17 11:43:30 -1000

    Put down ideas for site info on landing page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -10,6 +10,9 @@ const Landing = () => (
       <Col xs={6}>
         <h1>Welcome to Ohana Scholars!</h1>
         <h5>Looking a study partner for your upcoming midterms and finals?</h5>
+        {/* Search for courses you're taking */}
+        {/* Find out who else is looking for a study buddy or group! */}
+        {/* Join study sessions or create your own! */}
         <h5>Get started by clicking below!</h5>
         <Link to="/signup"><Button className="pink-btn">Create an account</Button></Link>
         <p>Already have an account? <Link to="/signin">Sign in</Link></p>
