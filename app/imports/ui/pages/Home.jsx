2de24d0016523949dd4445313248b69a41b57ffc
aldenparoni <aparoni@hawaii.edu>
2023-04-17 11:34:42 -1000

    Changed button styling, started rearranging landing and home page

--- app/imports/ui/pages/Home.jsx
+++ app/imports/ui/pages/Home.jsx
@@ -1,19 +1,40 @@
 import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { useTracker } from 'meteor/react-meteor-data';
 import { Col, Container, Row, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { Student } from '../../api/student/Student';
+import LoadingSpinner from '../components/LoadingSpinner';
 
-/* A simple static component to render some text for the landing page. */
-const Home = () => (
-  <Container id="landing-page" fluid className="py-3">
-    <Row id="landing" className="align-items-center justify-content-center">
-      <Col xs={8} className="text-center">
-        <h1>Hello!</h1>
-        <Link to="/add"><Button className="pink-btn">Create a session</Button></Link>
-        <Link to="/list"><Button className="pink-btn">Browse sessions</Button></Link>
-        <Link to="/courses"><Button className="pink-btn">Browse courses</Button></Link>
-      </Col>
-    </Row>
-  </Container>
-);
+/* Try to figure out how to incorporate user's first name in home page. */
+const Home = () => {
+  // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
+  const { ready, student } = useTracker(() => {
+    // Note that this subscription will get cleaned up
+    // when your component is unmounted or deps change.
+    // Get access to Student documents.
+    const subscription = Meteor.subscribe(Student.userPublicationName);
+    // Determine if the subscription is ready
+    const rdy = subscription.ready();
+    // Get the Student documents
+    const studentItems = Student.collection.find({}).fetch();
+    return {
+      student: studentItems,
+      ready: rdy,
+    };
+  }, []);
+  return (ready ? (
+    <Container id="landing-page" fluid className="py-3">
+      <Row id="landing" className="align-items-center justify-content-center">
+        <Col xs={8} className="text-center">
+          <h1>Hello!</h1>
+          <Link to="/add"><Button className="pink-btn">Create a session</Button></Link>
+          <Link to="/list"><Button className="pink-btn">Browse sessions</Button></Link>
+          <Link to="/courses"><Button className="pink-btn">Browse courses</Button></Link>
+        </Col>
+      </Row>
+    </Container>
+  ) : <LoadingSpinner />);
+};
 
 export default Home;
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -5,13 +5,13 @@ import { Link } from 'react-router-dom';
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
   <Container id="landing-page" fluid className="py-3">
-    <Row id="landing" className="align-items-center justify-content-center">
-      <Col xs={8} className="text-center">
+    <Row id="landing" className="align-items-center">
+      <Col xs={1} />
+      <Col xs={6}>
         <h1>Welcome to Ohana Scholars!</h1>
-        <h5>Looking a study partner for your upcoming midterms and finals? Get started by clicking below!</h5>
-        <Link to="/signup">
-          <Button className="pink-btn">Create an account</Button>
-        </Link>
+        <h5>Looking a study partner for your upcoming midterms and finals?</h5>
+        <h5>Get started by clicking below!</h5>
+        <Link to="/signup"><Button className="pink-btn">Create an account</Button></Link>
         <p>Already have an account? <Link to="/signin">Sign in</Link></p>
       </Col>
     </Row>
