a5ad32f079fee19ae8742a6729e80727fe9ec619
aldenparoni <aparoni@hawaii.edu>
2023-04-19 12:00:53 -1000

    Create RateStudent.jsx

---
+++ app/imports/ui/pages/RateStudent.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Card, Col, Container, Row } from 'react-bootstrap';
+import { AutoForm, ErrorsField, SubmitField, TextField } from 'uniforms-bootstrap5';
+import swal from 'sweetalert';
+import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+import SimpleSchema from 'simpl-schema';
+import { Courses } from '../../api/courses/Courses';
+
+// Create a schema to specify the structure of the data to appear in the form.
+const formSchema = new SimpleSchema({
+  rating: Number,
+  reason: String,
+});
+
+const bridge = new SimpleSchema2Bridge(formSchema);
+
+/* Renders the AddStuff page for adding a document. */
+const RateStudent = () => {
+
+  // On submit, insert the data.
+  const submit = (data, formRef) => {
+    const { subject, title, name } = data;
+    Courses.collection.insert(
+      { subject, title, name },
+      (error) => {
+        if (error) {
+          swal('Error', error.message, 'error');
+        } else {
+          swal('Success', 'Course added successfully', 'success');
+          formRef.reset();
+        }
+      },
+    );
+  };
+
+  // Render the form. Use Uniforms: https://github.com/vazco/uniforms
+  let fRef = null;
+  return (
+    <Container className="py-3">
+      <Row className="justify-content-center">
+        <Col xs={5}>
+          <Col className="text-center"><h2>Add Course</h2></Col>
+          <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => submit(data, fRef)}>
+            <Card>
+              <Card.Body>
+                <Row>
+                  <Col><TextField name="subject" /></Col>
+                  <Col><TextField name="title" /></Col>
+                </Row>
+                <TextField name="name" />
+                <SubmitField value="Submit" />
+                <ErrorsField />
+              </Card.Body>
+            </Card>
+          </AutoForm>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default RateStudent;
