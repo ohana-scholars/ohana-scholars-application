68535b912f617088058432a64783b22c65b2788d
audreysoares <amsoares@hawaii.edu>
2023-04-24 11:16:21 -1000

    still trying to make the delete button work

--- app/imports/ui/components/SessionAdmin.jsx
+++ app/imports/ui/components/SessionAdmin.jsx
@@ -1,49 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Card from 'react-bootstrap/Card';
-import { Col, Image, ListGroup, Row, Button, Modal } from 'react-bootstrap';
+import { Col, Image, ListGroup, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Participant from './Participant';
-import { Sessions } from '../../api/sessions/Sessions';
-
-// When the 'remove' button is clicked on a chat message, delete that message.
-const deletesCardMaybe = (session) => {
-  Sessions.remove(session._id);
-};
-
-const DeleteButton = () => {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
-  return (
-    <>
-      <Button variant="primary" onClick={handleShow}>
-        Delete
-      </Button>
-
-      <Modal
-        show={show}
-        onHide={handleClose}
-        backdrop="static"
-        keyboard={false}
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>Are you sure you want to delete this session?</Modal.Title>
-        </Modal.Header>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={deletesCardMaybe}>
-            Delete
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  );
-};
+import DeleteButton from './DeleteSessionButton';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 const SessionAdmin = ({ session, participants }) => (
