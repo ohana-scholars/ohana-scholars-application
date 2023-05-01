0ccaf1f429596de8732577e86a9068e462258d2d
aldenparoni <aparoni@hawaii.edu>
2023-05-01 09:10:21 -1000

    fixed navbar logo, added button to return to profile

--- app/imports/ui/pages/ListReviews.jsx
+++ app/imports/ui/pages/ListReviews.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Container, Row, Table } from 'react-bootstrap';
+import { Col, Container, Row, Table, Button } from 'react-bootstrap';
 import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Reputation } from '../../api/reputation/Reputation';
@@ -25,13 +25,16 @@ const ListReviews = () => {
   }, []);
   return (ready ? (
     <Container className="py-3" id="rate-student-page">
-      <Row className="justify-content-center">
-        <Col xs={5}>
-          <Col className="text-center"><h2>{student[0].firstName}&apos;s Reviews</h2></Col>
+      <Row className="justify-content-center align-items-center">
+        <Col xs={8}>
+          <Col className="text-center">
+            <h2>{student[0].firstName}&apos;s Reviews</h2>
+            <Button className="pink-btn home-page-btn">Return to profile</Button>
+          </Col>
           <Table striped bordered hover>
             <thead>
               <tr>
-                <th>Owner</th>
+                <th>User</th>
                 <th>Rating</th>
                 <th>Feedback</th>
               </tr>
