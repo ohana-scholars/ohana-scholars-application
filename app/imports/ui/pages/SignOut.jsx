4299afc895c0332981ea2344e17ed0468a1b2bb6
aldenparoni <aparoni@hawaii.edu>
2023-05-01 18:50:42 -1000

    styled sign out page and changed font to Poppins

--- app/imports/ui/pages/SignOut.jsx
+++ app/imports/ui/pages/SignOut.jsx
@@ -2,20 +2,24 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Button, Col, Container, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import { ArrowUpLeftSquareFill } from 'react-bootstrap-icons';
+import { BoxArrowLeft, PersonFill } from 'react-bootstrap-icons';
 
 /* After the user clicks the "SignOut" link in the NavBar, log them out and display this page. */
 const SignOut = () => {
   Meteor.logout();
   return (
     <Container fluid className="py-3">
-      <Row className="align-items-center justify-content-center">
+      <Row className="align-items-center justify-content-center" id="sign-out">
         <Col id="signout-page" className="text-center py-3">
-          <h2>You are signed out. Thank you for using Ohana Scholars!</h2>
+          <h2>You are now signed out. Thank you for using Ohana Scholars!</h2>
           <Link to="/">
-            <Button className="pink-btn btn-lg home-page-btn">
-              <ArrowUpLeftSquareFill size={40} className="px-2" />
-              Return to the landing page
+            <Button className="pink-btn home-page-btn">
+              <BoxArrowLeft size={40} className="px-2" /> Return to the landing page
+            </Button>
+          </Link>
+          <Link to="/signin">
+            <Button className="pink-btn home-page-btn">
+              <PersonFill size={40} className="px-1" /> Log in
             </Button>
           </Link>
         </Col>
