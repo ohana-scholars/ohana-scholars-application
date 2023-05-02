591d155d479a93ae221873e334aa7c511dc66075
tungxn <tungxn@hawaii.edu>
2023-05-01 20:31:31 -1000

    update email formating

--- app/server/main.js
+++ app/server/main.js
@@ -18,7 +18,7 @@ Meteor.methods({
       from: 'OhanaScholar@email.com',
       subject: `Joined Session: ${sessionName}`,
       // eslint-disable-next-line max-len
-      text: `You have registered for the session ${sessionName} in ${sessionLocation} at ${sessionDate.getMonth() + 1}/${sessionDate.getDate()}/${sessionDate.getFullYear()} ${sessionTime}. Please remember to ${notes}`,
+      text: `You have registered for the session ${sessionName} in ${sessionLocation} at ${sessionDate.getMonth() + 1}/${sessionDate.getDate()}/${sessionDate.getFullYear()} ${sessionTime}. Please mark your calendar. Please remember: ${notes}`,
     });
   },
 
