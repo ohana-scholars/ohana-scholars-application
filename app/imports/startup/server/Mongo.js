0399392838257b54edc4d9999cadea5d74d573c2
Mark Burgess <mkh.burg@gmail.com>
2023-05-01 18:51:53 -1000

    Deleted All Stuff related files

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -7,21 +7,6 @@ import { Emails } from '../../api/email/Email';
 import { Reputation } from '../../api/reputation/Reputation';
 
 /* eslint-disable no-console */
-
-// Initialize the database with a default data document.
-const addData = (data) => {
-  console.log(`  Adding: ${data.name} (${data.owner})`);
-  Stuffs.collection.insert(data);
-};
-
-// Initialize the StuffsCollection if empty.
-if (Stuffs.collection.find().count() === 0) {
-  if (Meteor.settings.defaultData) {
-    console.log('Creating default data.');
-    Meteor.settings.defaultData.forEach(data => addData(data));
-  }
-}
-
 // Initialize the database with courses
 const addCourses = (course) => {
   console.log(`  Adding: ${course.name}`);
